db46069ba299f7b730ce9bef853b8446ed95fe72
smartful <remi_rodrigues@hotmail.com>
2022-10-29 03:52:05 +0200

    ajout base page Product et Order

---
+++ orderList.php
@@ -0,0 +1,19 @@
+<?php
+require("./layout/htmlHead.php");
+session_start();
+echo htmlHead("Order", "style");
+?>
+    <body>
+    <?php include("./layout/header.php"); ?>
+    <?php include("deconnexionMenu.php"); ?>
+
+    <!-- le menu des activités -->
+    <?php include("themesMenu.php"); ?>
+
+    <div id="corps">
+        <h1>Product Order</h1>
+        <h2>Order</h2>
+    </div>
+    <?php include("./layout/footer.php"); ?>
+    </body>
+</html>
