77fcab51509d6f066478636b0526477807ecb6d7
smartful <remi_rodrigues@hotmail.com>
2024-12-05 03:44:40 +0100

    factorisation du menu de la premiere page

--- menu.php
+++ menu.php
@@ -1,9 +1,7 @@
 <div id="menu">
    <div class="element_menu">
      <h3>Product Order</h3>
-     <ul>
-       <li><a href="index.php">Home</a></li>
-       <li><a href="registerForm.php">Inscription</a></li>
-     </ul>
+      <a href="index.php">Home</a>
+      <a href="registerForm.php">Inscription</a>
    </div>
 </div>
